d63605fdbb47ff18f05fbd499d614281b760abe7
Ajay Yadav <ajju.kvmc@gmail.com>
2015-03-28 16:01:47 +0530

    adding update in views

--- photoShare/urls.py
+++ photoShare/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.main, name='main'),
+	url(r'^(\d+)/$', views.album, name='album'),
+	url(r'^image/(\d+)/$', views.image, name='image'),
+	url(r'^(\d+)/(full|thumbnails)/$', views.album, name='album'),
+	url(r'^(\d+)/(full|thumbnails|edit)/$', views.album, name='album'),
+	url(r'^update/$', views.update, name='update')
 )
--- photoShare/views.py
+++ photoShare/views.py
@@ -30,14 +30,18 @@ def main(request):
 
 	return render_to_response('photo/list.html', dict(albums=albums, user=request.user, media_url=MEDIA_URL))
 
-def album(request, pk):
+def album(request, pk, view="thumbnails"):
 	"""Album listing."""
+	
+	num_images = 30
+	if view == "full": num_images = 10
+
 	album = Album.objects.get(pk=pk)
 	if not album.public and not request.user.is_authenticated():
 		return HttpResponse("Error: you need to be logged in to view this album.")
 	
 	images = album.image_set.all()
-	paginator = Paginator(images, 30)
+	paginator = Paginator(images, num_images)
 	try: page = int(request.GET.get("page", '1'))
 	except ValueError: page = 1
 
@@ -46,4 +50,49 @@ def album(request, pk):
 	except (InvalidPage, EmptyPage):
 		images = paginator.page(paginator.num_pages)
 
-	return render_to_response("photo/album.html", dict(album=album, images=images, user=request.user, media_url=MEDIA_URL))
+	# add list of tags as string and list of album objects to each image object
+	for img in images.object_list:
+		tags = x[x[1] for x in img.albums.values_list()]
+		img.tag_lst = join(tags, ', ')
+		img.album_lst = [x[1] for x in img.albums.values_list()]
+	
+	d = dict(album=album, images=images, user=request.user, view=view, albums=Album.objects.all(), media_url=MEDIA_URL)
+	d.update(csrf(request))
+	return render_to_response("photo/album.html", d)
+
+def image(request, pk):
+	"""Image page."""
+	img = Image.objects.get(pk=pk)
+	return render_to_response("poto/image.html", dict(image=img, user=request.user, backurl=request.META["HTTP_REFERER"], media_url=MEDIA_URL))
+
+def update(request):
+	"""Update image title, rating, tags, albums."""
+	p = request.POST
+	images = default.dict(dict)
+
+	# create dictionary of properties for each image
+	for k, v in p.items():
+		if k.startswith("title") or k.startswith("rating") or k.startswith("tags"):
+			k, pk = k.split('-')
+			images[pk][k] = v
+		elif k.startswith("album"):
+			pk = k.split('-')[1]
+			images[pk]["albums"] = p.getlist(k)
+
+	# process properties, assign to image objects and save
+	for k, d in images.items():
+		image = Image.objects.get(pk=k)
+		image.title = d["title"]
+		image.rating = d["rating"]
+	
+		# tags - assign or create if a new tag!
+		tags = d["tags"].split(', ')
+		lst = []
+		for t in tags:
+			if t: lst.append(Tag.objects.get_or_create(tag=t)[0])
+		
+		if "albums" in d:
+			image.albums = d["albums"]
+		image.save()
+
+	return HttpResponseRedirect(request.META["HTTP_REFERER"], dict(media_url=MEDIA_URL))
