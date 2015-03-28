f0f68822dc7553e91401c65bfdcb2c128a870d78
Ajay Yadav <ajju.kvmc@gmail.com>
2015-03-29 03:24:01 +0530

    changes in templates

--- photo/settings.py
+++ photo/settings.py
@@ -85,3 +85,23 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = '/home/ajay/Desktop/Django/photo/media'
 MEDIA_URL = 'http://127.0.0.1:8000/media/'
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = 'staticfiles'
+
+DEBUG = False
+
+try:
+	from .local_settings import *
+except ImportError:
+	pass
+
+STATICFILES_DIRS = (
+	os.path.join(BASE_DIR, "static"),
+)
